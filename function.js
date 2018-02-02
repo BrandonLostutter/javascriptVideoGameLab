6cfc1ecc023357cfef13b4b5439f253c5f92b8a3
Brandon Lostutter <blostutter@gmail.com>
2018-02-01 19:14:43 -0500

    New getDamage Function

--- function.js
+++ function.js
@@ -1,42 +1,54 @@
-var enemyName = "JavaScript Asshole Monster";
-    var userHealth = 40;
-    var enemyHealth = 10;
-    var wins = 0;
-  
-  function startGame(){
-      var r = confirm("Would You Like to Play?");
-    if (r === true) {
-        var userName = prompt("What is Your Username?");
-        startCombat();
-   
-  
-  function startCombat(){
-    while (userHealth > 0 && wins < 3){
-      console.log(userName + ' has ' + userHealth + ' health left.');
-      console.log(enemyName + ' has ' + enemyHealth + ' health left.');
-      getDamage(userHealth);
-      getDamage(enemyHealth);
-      enemyHealth -= Math.floor((Math.random() * 2) + 1);
-      if(userHealth <= 0){
-        console.log("GAME OVER");
-        break;
-      } else if(enemyHealth <= 0){
-        wins++; 
-        enemyHealth = 10; 
-        console.log("WIN!", wins);
-      } else if(wins === 3){
-         console.log('You have defeated ' + enemyName + '!');
-         break;
+
+      var enemyName = "JavaScript Monster";
+      var userHealth = 40;
+      var enemyHealth = 10;
+      var wins = 0;
+    
+    function startGame(){
+        var r = confirm("Would You Like to Play?");
+      if (r === true) {
+          var userName = prompt("What is Your Username?");
+          startCombat();
+     
+    
+    function startCombat(){
+      while (userHealth > 0 && wins < 3){
+        console.log(userName + ' has ' + userHealth + ' health left.');
+        console.log(enemyName + ' has ' + enemyHealth + ' health left.');
+        userHealth = getDamage(userHealth);
+        enemyHealth = getDamage(enemyHealth);
+        if(userHealth <= 0){
+          console.log("GAME OVER");
+          break;
+        } else if(enemyHealth <= 0){
+          wins++; 
+          enemyHealth = 10; 
+          console.log("WIN!", wins);
+        } else if(wins === 3){
+           console.log('You have defeated ' + enemyName + '!');
+           break;
+        }
       }
     }
-  }
-  
-  function getDamage(playerHealth){
-    var playerDamage = playerHealth -= Math.floor((Math.random() * 5) + 1);
-    return playerDamage;
-  }
-  
- } else {
-        console.log("See Ya!");
-    }
+    
+    // function getDamage(playerHealth){
+    //   var playerDamage = playerHealth -= Math.floor((Math.random() * 5) + 1);
+    //   return playerDamage
+    // }
+    
+     function getDamage(playerHealth){
+        var attack = Math.floor((Math.random() * 5) + 1);
+        return playerHealth -= attack;
+     }
+    
+   } else {
+          console.log("See Ya!");
+      }
+ 
   }startGame();
+  
+  
+ 
+    
+      // var attack = Math.floor((Math.random() * 5) + 1);
+      // return (playerHealth -= attack)
